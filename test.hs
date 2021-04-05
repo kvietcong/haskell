3f8ac18c1dcdc718ee1b431d75bc827dbd991a3d
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2021-04-05 14:45:05 -0700

    � Add some testing

--- test.hs
+++ test.hs
@@ -1,2 +1,14 @@
 doubleMe x = 2 * x
 doubleUs x y = doubleMe x + doubleMe y
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "You okay?"
+maximum' [x] = x
+maximum' (x:y) = max x (maximum' y)
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "You okay?"
+maximum'' [x] = x
+maximum'' (h:t)
+    | h > maximum'' t = h
+    | otherwise = maximum'' t
