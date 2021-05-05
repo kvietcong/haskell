e131b2ef6613caf3b30d910a733c103c0806e48c
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2021-05-05 00:03:40 -0700

    🤏 Small improvements

--- HeathrowLondon.hs
+++ HeathrowLondon.hs
@@ -1,7 +1,13 @@
 import Control.Monad (sequence)
 
 data Choice = Choice Int Int Int deriving Show
-data Side = Top Int | Bottom Int deriving Show
+data Side = Top Int | Cross Int | Bottom Int deriving Show
+
+fromSide side = case side of
+                  Top a -> a
+                  Cross a -> a
+                  Bottom b -> b
+
 
 inputs = [[0    , 50    , 10 ]
          ,[30   , 5     , 90 ]
@@ -11,21 +17,50 @@ inputs = [[0    , 50    , 10 ]
 choices = map inputToChoice inputs
     where inputToChoice input = let [a, b, c] = input in Choice a b c
 
-path = tail . reverse $ choose [Top 0] choices
+path = dropWhile (\x -> fromSide x == 0) $ reverse $ choose [Top 0] choices
     where choose steps [] = steps
           choose steps (Choice cross top bot:choices) =
               case step of
                 Top a -> if top < crossBot
                              then choose (Top top:steps) choices
-                             else choose (Bottom crossBot:steps) choices
+                             else choose (Bottom bot:Cross cross:steps) choices
                 Bottom a -> if bot < crossTop
                              then choose (Bottom bot:steps) choices
-                             else choose (Top crossTop:steps) choices
+                             else choose (Top top:Cross cross:steps) choices
                 where step = head steps
                       crossTop = top + cross
                       crossBot = bot + cross
 
 price = sum $ map fromSide path
-    where fromSide side = case side of
-                            Top a -> a
-                            Bottom b -> b
+
+------------------------------------
+-- Learn You a Haskell's Solution --
+------------------------------------
+data Section = Section { getA :: Int, getB :: Int, getC :: Int } deriving (Show)  
+type RoadSystem = [Section]  
+
+data Label = A | B | C deriving (Show)  
+type Path = [(Label, Int)]
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)  
+roadStep (pathA, pathB) (Section a b c) =   
+    let priceA = sum $ map snd pathA  
+        priceB = sum $ map snd pathB  
+        forwardPriceToA = priceA + a  
+        crossPriceToA = priceB + b + c  
+        forwardPriceToB = priceB + b  
+        crossPriceToB = priceA + a + c  
+        newPathToA = if forwardPriceToA <= crossPriceToA  
+                        then (A,a):pathA  
+                        else (C,c):(B,b):pathB  
+        newPathToB = if forwardPriceToB <= crossPriceToB  
+                        then (B,b):pathB  
+                        else (C,c):(A,a):pathA  
+    in  (newPathToA, newPathToB)
+
+optimalPath :: RoadSystem -> Path  
+optimalPath roadSystem = 
+    let (bestAPath, bestBPath) = foldl roadStep ([],[]) roadSystem  
+    in  if sum (map snd bestAPath) <= sum (map snd bestBPath)  
+            then reverse bestAPath  
+            else reverse bestBPath
