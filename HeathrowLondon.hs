01fb24b36123740350ddd874b5318065859ae571
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2021-05-05 18:42:39 -0700

    📝 Add some documentation to the Heathrow London Problem

--- HeathrowLondon.hs
+++ HeathrowLondon.hs
@@ -1,20 +1,42 @@
-data Section = Section Int Int Int deriving Show
-data Choice = Top Int | Cross Int | Bottom Int deriving Show
+{-|
+   Data structure that represents the price to choose the
+   cross, top, or bottom path (In that order respectively)
+-}
+data Section = Section { crossPrice :: Int
+                       , topPrice :: Int
+                       , bottomPrice :: Int }
+                       deriving Show
 
-fromSide :: Choice -> Int
-fromSide side = case side of
-                  Top a -> a
-                  Cross a -> a
-                  Bottom b -> b
+{-|
+   Data structure that represents the choice taken when at
+   a section and its associated cost
+-}
+data Choice =  Cross Int | Top Int | Bottom Int deriving Show
 
+{-|
+   Unwraps a Choice value into the amount of
+   effort it took for that choice
+-}
+fromChoice :: Choice -> Int
+fromChoice side = case side of
+                    Cross a -> a
+                    Top a -> a
+                    Bottom a -> a
+
+{-|
+   The Heathrow to London Road System found
+   in the Functionally Solving Problems chapter of
+   the Learn You a Haskell Book
+-}
 roadSystem :: [Section]
 roadSystem = [ Section 0  50 10 
              , Section 30 5  90 
              , Section 20 40 2  
              , Section 25 10 8 ]
 
-path :: [Choice]
-path = dropWhile ((== 0) . fromSide) $ (reverse . choose [Top 0]) roadSystem
+-- |Retrives the best choices to minimize the price associated
+bestPath :: [Section] -> [Choice]
+bestPath = dropWhile ((== 0) . fromChoice) . reverse . choose [Top 0]
     where choose choices [] = choices
           choose choices (Section cross top bot:sections) =
               case head choices of
@@ -26,22 +48,26 @@ path = dropWhile ((== 0) . fromSide) $ (reverse . choose [Top 0]) roadSystem
                                else choose (Top top:Cross cross:choices) sections
                 Cross _ -> error "This shouldn't happen"
 
-price :: Int
-price = sum $ fmap fromSide path
+-- |Retrieves the total price of a given set of choices
+price :: [Choice] -> Int
+price = sum . fmap fromChoice
 
+-- |Entry point that prints out the solutions
 main :: IO ()
 main = do
-    putStrLn "This is the Road System"
-    print roadSystem
-    putStrLn $ '\n':"This is the Path"
-    print path
-    putStrLn $ '\n':"This is the Price"
-    print price
+    let path = bestPath roadSystem
 
-{-
+    putStrLn $ "The Road System: " ++ show roadSystem
+
+    putStrLn $ '\n':"The best path is: " ++ show path
+
+    putStrLn $ '\n':"This is the best price: " ++
+        (show . price) path ++ " units"
+
 ------------------------------------
 -- Learn You a Haskell's Solution --
 ------------------------------------
+{-
 data Section' = Section' { getA :: Int, getB :: Int, getC :: Int } deriving (Show)  
 type RoadSystem = [Section']  
 
